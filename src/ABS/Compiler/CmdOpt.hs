724a5e56d724e9ee12889a44fa446333c8413a3b
Nikolaos Bezirgiannis <bezirg@gmail.com>
2016-03-29 19:11:09 +0200

    Added simple code for habs executable

--- src/ABS/Compiler/CmdOpt.hs
+++ src/ABS/Compiler/CmdOpt.hs
@@ -11,7 +11,7 @@ cmdOpt = unsafePerformIO (cmdArgs cmdOptSpec)
 
 data CmdOpt = CmdOpt {
       src_files :: [FilePath]     -- ^ The input ABS module files (ending in .abs) or ABS directories
-    , output_dir :: Maybe FilePath -- ^ In which directory to put all the Haskell translated files (.hs files)
+    , output_dir :: FilePath -- ^ In which directory to put all the Haskell translated files (.hs files)
     , create_script :: Bool -- ^ creates a helper bash script for easier invoking the ghc Haskell compiler and puts it in the outputdir
     , dump_ast :: Bool            -- ^ A flag to dump the parsed AST to stderr
     } deriving (Show, Data, Typeable)
@@ -19,7 +19,7 @@ data CmdOpt = CmdOpt {
 cmdOptSpec :: CmdOpt
 cmdOptSpec = CmdOpt { 
                src_files = def &= args &= typ "FILES/DIRS"
-             , output_dir = def &= typDir &= help "In which directory to put all the Haskell translated files (.hs files)" 
+             , output_dir = "gen/haskell" &= typDir &= help "In which directory to put all the Haskell translated files (.hs files)" 
              , create_script = def &= help "If given, creates a helper bash script for easier invoking the ghc Haskell compiler."
              , dump_ast = def &= help "A flag to dump the parsed AST to stderr"
              }
--- src/habs.hs
+++ src/habs.hs
@@ -1,10 +1,35 @@
 module Main where
 
-import ABS.Compiler.CmdOpt
-import Control.Exception (evaluate)
+import qualified ABS.AST as ABS (Program (Prog))
+import qualified Language.Haskell.Exts.Syntax as HS (Module (..), ModuleName (..))
+import qualified Language.Haskell.Exts.Pretty as HS (prettyPrint)
+import ABS.Parser
 
--- | main
+import ABS.Compiler.CmdOpt
+import ABS.Compiler.Firstpass.SymbolTable
+import ABS.Compiler.Codegen.Mod (tModul)
+
+import System.FilePath ((</>))
+import System.Directory (createDirectoryIfMissing)
+
+-- | The habs translator executable
 main :: IO ()
-main = do
-  _ <- evaluate cmdOpt           -- force the cmdopt parsing, otherwise will not run even --help
-  return ()
+main = 
+  if null (src_files cmdOpt)
+  then error "No ABS files to translate are given as input. Try --help"
+  else do
+     -- 0. parse all modules to their ASTs
+     modules <- concatMap (\ (_,Ok (ABS.Prog ms)) -> ms) . concat <$> mapM parseFileOrDir (src_files cmdOpt)
+     -- 1.st-pass: build the symboltables
+     let symbolTables = globalSTs modules
+     -- 2.nd-pass: generate code
+     mapM_ (\ m -> ppHaskellFile $ tModul m symbolTables)  modules
+
+-- | pretty-print a Haskell-AST to a file ".hs"
+ppHaskellFile :: HS.Module -> IO ()
+ppHaskellFile m@(HS.Module _ (HS.ModuleName s) _ _ _ _ _) = do
+  let haskellFileName = map (\ c -> if c == '.' then '/' else c) s  ++ ".hs"
+  let haskellDirName = output_dir cmdOpt
+  createDirectoryIfMissing True haskellDirName -- with True, it's like mkdir -p
+  writeFile (haskellDirName </> haskellFileName) $ HS.prettyPrint m
+
