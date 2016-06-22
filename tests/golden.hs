c6da49fe200016e97fa137dc646185486244e27a
Nikolaos Bezirgiannis <bezirg@gmail.com>
2016-06-22 10:08:24 +0200

    Attempt to fix travis test build

--- tests/golden.hs
+++ tests/golden.hs
@@ -17,7 +17,6 @@ import ABS.Compiler.Utils (showQU)
 import ABS.Compiler.Firstpass.SymbolTable (globalSTs)
 import qualified Language.Haskell.Exts.Pretty as HS (prettyPrintWithMode, defaultMode, linePragmas)
 
-import System.Environment (getArgs, withArgs)
 import System.Directory (getDirectoryContents, doesDirectoryExist, createDirectoryIfMissing)
 import System.FilePath ((</>), (<.>), dropExtension)
 import Data.List (isSuffixOf,isInfixOf)
@@ -28,50 +27,46 @@ hsOutputDir = "dist"</>"test"</>"gen"</>"haskell"
 
 main :: IO ()
 main = do
-    args <- getArgs -- append to stdin args
+  createDirectoryIfMissing True hsOutputDir
 
+  to_pass <- map dropExtension . filter (".abs" `isSuffixOf`) <$> getDirectoryContents ("habs-samples"</>"to_pass")
+  to_fail <- map dropExtension . filter (".abs" `isSuffixOf`) <$> getDirectoryContents ("habs-samples"</>"to_fail")
+  to_timeout <- map dropExtension . filter (".abs" `isSuffixOf`) <$> getDirectoryContents ("habs-samples"</>"to_timeout")
+  not_compile <- map dropExtension . filter (".abs" `isSuffixOf`) <$> getDirectoryContents ("habs-samples"</>"not_compile")
 
-    createDirectoryIfMissing True hsOutputDir
+  isSandboxed <- doesDirectoryExist ".cabal-sandbox"
+  let (ghc,ghcExtraArgs) = if isSandboxed then ("cabal",["exec","ghc","--"]) else ("ghc",[])
+  let ghcArgs sample = ghcExtraArgs ++ [ "--make"
+                                       , "-O0"
+                                       , "-fforce-recomp"
+                                       , "-main-is", sample
+                                       , hsOutputDir </> sample <.> "hs"]
 
-    to_pass <- map dropExtension . filter (".abs" `isSuffixOf`) <$> getDirectoryContents ("habs-samples"</>"to_pass")
-    to_fail <- map dropExtension . filter (".abs" `isSuffixOf`) <$> getDirectoryContents ("habs-samples"</>"to_fail")
-    to_timeout <- map dropExtension . filter (".abs" `isSuffixOf`) <$> getDirectoryContents ("habs-samples"</>"to_timeout")
-    not_compile <- map dropExtension . filter (".abs" `isSuffixOf`) <$> getDirectoryContents ("habs-samples"</>"not_compile")
+  defaultMainWithIngredients (htmlRunner:defaultIngredients) $
+   localOption (mkTimeout 10000000) $ -- timeouts any individual test **case** at 10s
+    testGroup "habs" [
+      testGroup "to_pass" [ testGroup "transpile" $ map (\ sample -> testCase sample $ transpile ("habs-samples"</>"to_pass") sample) to_pass
+                          , testGroup "compile" $ map (\ sample -> testProgram sample ghc (ghcArgs sample) Nothing) to_pass
+                          , testGroup "execute" $ map (\ sample -> goldenVsFile sample
+                                                                        ("habs-samples"</>"to_pass"</>sample<.>"out")
+                                                                        (hsOutputDir</>sample<.>"out")
+                                                                        (exec_stdout sample)
+                                                         ) to_pass]
+    , testGroup "to_fail" [ testGroup "transpile" $ map (\ sample -> testCase sample $ transpile ("habs-samples"</>"to_fail") sample) to_fail
+                          , testGroup "compile" $ map (\ sample -> testProgram sample ghc (ghcArgs sample) Nothing) to_fail
+                          , testGroup "execute" $ map (\ sample -> testCase sample $ exec_stderr sample
+                                                         ) to_fail]
 
-    isSandboxed <- doesDirectoryExist ".cabal-sandbox"
-    let (ghc,ghcExtraArgs) = if isSandboxed then ("cabal",["exec","ghc","--"]) else ("ghc",[])
-    let ghcArgs sample = ghcExtraArgs ++ [ "--make"
-                                         , "-O0"
-                                         , "-fforce-recomp"
-                                         , "-main-is", sample
-                                         , hsOutputDir </> sample <.> "hs"]
-
-    withArgs ("-j1":"--catch-stderr":args) $ -- don't run tests in parallel because it messes output
-       defaultMainWithIngredients (htmlRunner:defaultIngredients) $
-        localOption (mkTimeout 10000000) $ -- timeouts any individual test **case** at 10s
-         testGroup "habs" [
-           testGroup "to_pass" [ testGroup "transpile" $ map (\ sample -> testCase sample $ transpile ("habs-samples"</>"to_pass") sample) to_pass
-                               , testGroup "compile" $ map (\ sample -> testProgram sample ghc (ghcArgs sample) Nothing) to_pass
-                               , testGroup "execute" $ map (\ sample -> goldenVsFile sample
-                                                                            ("habs-samples"</>"to_pass"</>sample<.>"out")
-                                                                            (hsOutputDir</>sample<.>"out")
-                                                                            (exec_stdout sample)
-                                                             ) to_pass]
-         , testGroup "to_fail" [ testGroup "transpile" $ map (\ sample -> testCase sample $ transpile ("habs-samples"</>"to_fail") sample) to_fail
-                               , testGroup "compile" $ map (\ sample -> testProgram sample ghc (ghcArgs sample) Nothing) to_fail
-                               , testGroup "execute" $ map (\ sample -> testCase sample $ exec_stderr sample
-                                                             ) to_fail]
-  
-         , testGroup "to_timeout" [ testGroup "transpile" $ map (\ sample -> testCase sample $ transpile ("habs-samples"</>"to_timeout") sample) to_timeout
-                                  , testGroup "compile" $ map (\ sample -> testProgram sample ghc (ghcArgs sample) Nothing) to_timeout
-                                  , testGroup "execute" $ map (\ sample -> expectFail $ testCase sample $ exec_stdout sample) to_timeout
-                                  ]
-         , testGroup "not_compile" $ map (\ sample -> expectFail $ 
-                                     withResource (transpile ("habs-samples"</>"not_compile") sample `catch` (\ (_ :: SomeException) -> return ())) 
-                                                      (const $ print sample)
-                                                      (const $ testProgram sample ghc (ghcArgs sample) Nothing)
-                                         ) not_compile                         
-                          ]
+    , testGroup "to_timeout" [ testGroup "transpile" $ map (\ sample -> testCase sample $ transpile ("habs-samples"</>"to_timeout") sample) to_timeout
+                             , testGroup "compile" $ map (\ sample -> testProgram sample ghc (ghcArgs sample) Nothing) to_timeout
+                             , testGroup "execute" $ map (\ sample -> expectFail $ testCase sample $ exec_stdout sample) to_timeout
+                             ]
+    , testGroup "not_compile" $ map (\ sample -> expectFail $ 
+                                 withResource (transpile ("habs-samples"</>"not_compile") sample `catch` (\ (_ :: SomeException) -> return ())) 
+                                                  (const $ print sample)
+                                                  (const $ testProgram sample ghc (ghcArgs sample) Nothing)
+                                     ) not_compile                         
+                     ]
 
 
 transpile parentDir sample = do
@@ -93,13 +88,13 @@ transpile parentDir sample = do
 
 exec_stdout sample = do
   print sample
-  (exitCode, outStr, _errStr) <- readCreateProcessWithExitCode ((proc (hsOutputDir</>sample) []) { cwd = Just hsOutputDir }) "" -- empty stdin
+  (exitCode, outStr, _errStr) <- readCreateProcessWithExitCode (proc (hsOutputDir</>sample) []) "" -- empty stdin
   case exitCode of
     ExitSuccess -> writeBinaryFile (hsOutputDir</>sample<.>"out") outStr
     _ -> fail "Failed to execute ABS program"
 
 exec_stderr sample = do
   print sample
-  (_exitCode, _outStr, errStr) <- readCreateProcessWithExitCode ((proc (hsOutputDir</>sample) ["-t"]) { cwd = Just hsOutputDir }) "" -- empty stdin
+  (_exitCode, _outStr, errStr) <- readCreateProcessWithExitCode (proc (hsOutputDir</>sample) ["-t"]) "" -- empty stdin
   writeBinaryFile (hsOutputDir</>sample<.>"err") errStr
   assertBool "Unexpected pass" ("Uncaught-Exception" `isInfixOf` errStr || "Assertion failed" `isInfixOf` errStr)
